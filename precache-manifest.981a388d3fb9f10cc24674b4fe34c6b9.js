e9dabd2e2041f808b1369393783c0563cb5c0425
ho-tonym <ho.tonym@gmail.com>
2020-08-18 20:13:53 -0400

    Updates

---
+++ precache-manifest.981a388d3fb9f10cc24674b4fe34c6b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "337aff47c3c7f71449636f41c6317e87",
+    "url": "/gaj/index.html"
+  },
+  {
+    "revision": "27a97a40fbabb502e6f1",
+    "url": "/gaj/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "4c61890b95b37af6fe1f",
+    "url": "/gaj/static/js/2.14305a18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gaj/static/js/2.14305a18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27a97a40fbabb502e6f1",
+    "url": "/gaj/static/js/main.3c577967.chunk.js"
+  },
+  {
+    "revision": "cdd55c1559bbe07f18d2",
+    "url": "/gaj/static/js/runtime-main.7eaf4a73.js"
+  }
+]);
