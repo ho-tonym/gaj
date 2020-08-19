2d88389ff2b0e78fd11203963ecf2fb9f082001c
ho-tonym <ho.tonym@gmail.com>
2020-08-19 14:21:14 -0400

    a

--- src/App.js
+++ src/App.js
@@ -2,21 +2,10 @@ import React, {useEffect, useState} from 'react';
 import styles from './App.module.css';
 
 function App() {
-  const [state, setState] = useState("ZA WARUDO");
-  const [squares, setSquares] = useState([])
+  const [state, setState] = useState("WORLD");
   useEffect(() => {
-    fetch('https://usercontribution.herokuapp.com/mcabrex/activity')
-    .then(response => response.json())
-    .then((d) => {
-      const x = Object.values(d.data[2020])
-      const tempArray = []
-      x.forEach(e=>{
-        tempArray.push(Object.values(e))
-      })
-      setSquares(tempArray.flat())
-    })
-    // const interval = setInterval(gaj, 4000);
-    // return () => clearInterval(interval);
+    const interval = setInterval(gaj, 4000);
+    return () => clearInterval(interval);
   }, []);
 
   function gaj(){
@@ -24,27 +13,9 @@ function App() {
     setTimeout(()=> { setState("WORLD"); }, 100);
   }
 
-  function cubes(){
-    return squares.map((e, i) => <span key={i} className={styles.square} style={ e ? {backgroundColor: "#FFBD75"} : {backgroundColor: "white" }}></span>)
-  }
-
-  function contributionDays(){
-    let count = 0
-    for (var i = 0; i < squares.length; i++) {
-      if(!squares[i]){
-        count++
-      }
-    }
-    return count
-  }
-
   return (
     <div className={styles.App}>
       <h1>{state}</h1>
-      <p className={styles.font}>Days of no contributions for MCABREX: {contributionDays()}</p>
-      <div className={styles.activityContainer}>
-        {cubes()}
-      </div>
     </div>
   );
 }
